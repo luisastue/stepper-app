1527fbdb3e73c36210c60923b43926a8202ff48b
Stueckelberger Luisa <Lui.Stu@its.local>
2020-01-16 09:23:54 +0100

    initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/dates/DatesOverviewViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.dates;
+
+import androidx.lifecycle.ViewModel;
+
+public class DatesOverviewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
