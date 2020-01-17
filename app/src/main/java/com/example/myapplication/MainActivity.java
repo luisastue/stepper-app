103500da0192914a52496898f5df8276ee6b98ef
Stueckelberger Luisa <Lui.Stu@its.local>
2020-01-17 14:15:16 +0100

    sensor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.example.myapplication.data.DBService;
 import com.example.myapplication.ui.chart.ChartOverview;
 import com.example.myapplication.ui.dates.DatesOverview;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.ui.ProgressBar.CircularProgressBar;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private Sensor mStep;
+    private int steps = 0;
+    CircularProgressBar circularProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DBService.getInstance().init(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -29,13 +44,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
-        CircularProgressBar circularProgressBar = findViewById(R.id.circularProgress);
+        circularProgressBar = findViewById(R.id.circularProgress);
         circularProgressBar.setProgress(62);
         circularProgressBar.setProgressColor(Color.rgb(51,181,189));
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int width = displayMetrics.widthPixels;
-        circularProgressBar.setMinimumWidth(width);
     }
 
     @Override
@@ -53,4 +64,25 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return true;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mStep = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.equals(mStep)){
+            steps++;
+            circularProgressBar.setProgress(steps);
+            if(steps % 100 == 0){
+                DBService.getInstance().updateSteps(steps);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
